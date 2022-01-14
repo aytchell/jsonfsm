e702aa28b83e04229feb4668c888c876765ac39c
Hannes Lerchl <hannes.lerchl@googlemail.com>
2022-01-14 12:37:56 +0100

    Added new unit tests to check error handling
    If a given DeviceCommand throws a RuntimeException when executed this
    has a very bad effect on the whole state machine.
    
    These new unit tests show the problem.

---
+++ src/test/java/com/github/aytchell/jsonfsm/compiler/ExceptionOnExecuteTest.java
@@ -0,0 +1,90 @@
+package com.github.aytchell.jsonfsm.compiler;
+
+import com.github.aytchell.jsonfsm.*;
+import com.github.aytchell.validator.exceptions.ValidationException;
+import lombok.Getter;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.Map;
+
+import static com.github.aytchell.jsonfsm.compiler.ExceptionMessageChecks.readResourceTextFile;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+// If a given DeviceCommand.execute() throws an exception this should be handled gracefully
+public class ExceptionOnExecuteTest {
+    @Test
+    void exceptionDuringFirstOnExit() throws CompilationException, ValidationException, IOException {
+        exceptionInBehavior("Exiting1");
+    }
+
+    @Test
+    void exceptionDuringSecondOnExit() throws CompilationException, ValidationException, IOException {
+        exceptionInBehavior("Exiting2");
+    }
+
+    @Test
+    void exceptionDuringFirstTransitionEffect() throws IOException, ValidationException, CompilationException {
+        exceptionInBehavior("Moving1");
+    }
+
+    @Test
+    void exceptionDuringSecondTransitionEffect() throws IOException, ValidationException, CompilationException {
+        exceptionInBehavior("Moving2");
+    }
+
+    @Test
+    void exceptionDuringFirstOnEntry() throws CompilationException, ValidationException, IOException {
+        exceptionInBehavior("Entering1");
+    }
+
+    @Test
+    void exceptionDuringSecondOnEntry() throws CompilationException, ValidationException, IOException {
+        exceptionInBehavior("Entering2");
+    }
+
+    private void exceptionInBehavior(String cmdPrefix) throws IOException, ValidationException, CompilationException {
+        final String json = readResourceTextFile("effects_everywhere.json");
+        final StateMachineCompiler compiler = StateMachineParser.parseAndListRequiredDeviceIds(json);
+        assertNotNull(compiler);
+
+        EvilDeviceCommandCompiler cmdCompiler = new EvilDeviceCommandCompiler(cmdPrefix);
+        final StateMachine machine = compiler.compileStateMachine(Map.of(5, cmdCompiler));
+        // this will trigger a transition with a bad effect
+        machine.injectEvent(3, "move ya");
+        // no exception should leave the above call and all the commands should have been executed
+        // (thrown exception does not interfere with the other effects)
+        assertEquals(6, cmdCompiler.getExecCounter());
+
+        /*
+        final String commandString = exception.getCommandString();
+        boolean commandAsExpected =
+                commandString.equals("Moving to 'Stop' ...") ^ commandString.equals("Still moving ...");
+        assertTrue(commandAsExpected);
+        assertMessageReadsLike(exception.getLocation(), List.of("transition", "Start", "Stop", "trigger", "move"));
+         */
+    }
+
+    private static class EvilDeviceCommandCompiler implements DeviceCommandCompiler {
+        final String prefix;
+        @Getter
+        int execCounter;
+
+        EvilDeviceCommandCompiler() {
+            this(null);
+        }
+
+        EvilDeviceCommandCompiler(String commandPrefix) {
+            this.prefix = commandPrefix;
+        }
+
+        @Override
+        public DeviceCommand compile(String commandString) throws CompilationException {
+            if (prefix == null || commandString.startsWith(prefix)) {
+                return () -> { ++execCounter; throw new RuntimeException("Booom!"); };
+            }
+            return () -> { ++execCounter; };
+        }
+    }
+}
